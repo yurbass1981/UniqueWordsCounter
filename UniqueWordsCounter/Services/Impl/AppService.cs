9d96c78ce4bd927a1965452e8c25dcbfd38ea522
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-20 20:27:45 +0300

    Clean up a bit

--- UniqueWordsCounter/Services/Impl/AppService.cs
+++ UniqueWordsCounter/Services/Impl/AppService.cs
@@ -8,33 +8,47 @@ namespace UniqueWordsCounter.Services.Impl
 {
     internal class AppService : IAppService
     {
+        private readonly char[] _separators = { ' ', ',', '.', '!', '?', ':', ';', '-', '*' };
+
         public void Run(string filePath, string resultFilePath)
         {
-            var keyValuePairs = new Dictionary<string, int>();
+            var uniqueWordsCountMap = GetUniqueWordsCountMapFromFile(filePath);
+            WriteResultToFile(resultFilePath, uniqueWordsCountMap);
+        }
+
+        private Dictionary<string, int> GetUniqueWordsCountMapFromFile(string filePath)
+        {
+            var uniqueWordsCountMap = new Dictionary<string, int>();
+
             var lines = File.ReadLines(filePath);
+                
 
             foreach (var line in lines)
             {
-                var words = line.Split(new char[] { ' ', ',', '.', '!', '?', ':', ';', '-' },
-                    StringSplitOptions.RemoveEmptyEntries);
+                var words = line.Split(_separators, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var word in words)
                 {
-                    if (!keyValuePairs.ContainsKey(word))
+                    if (!uniqueWordsCountMap.ContainsKey(word))
                     {
-                        keyValuePairs.Add(word, 1);
+                        uniqueWordsCountMap.Add(word, 1);
                     }
                     else
                     {
-                        keyValuePairs[word]++;
+                        uniqueWordsCountMap[word]++;
                     }
                 }
             }
 
+            return uniqueWordsCountMap;
+        }
+
+        private static void WriteResultToFile(string resultFilePath, Dictionary<string, int> uniqueWordsCountMap)
+        {
             using var sw = new StreamWriter(resultFilePath);
-            foreach (var row in keyValuePairs.OrderByDescending(x => x.Value))
-            {
-                sw.WriteLine($"{row.Key}, {row.Value}");
-            }
+            sw.WriteLine("Word | Count of usage");
+
+            foreach (var row in uniqueWordsCountMap.OrderByDescending(r => r.Value))
+                sw.WriteLine($"{row.Key} | {row.Value}");
         }
     }
 }
