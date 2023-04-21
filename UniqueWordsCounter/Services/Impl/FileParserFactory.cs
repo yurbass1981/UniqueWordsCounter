1a77ec9590c77f26136df72c04f878b13fb44147
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-21 10:45:35 +0300

    Added FileParserFactory

---
+++ UniqueWordsCounter/Services/Impl/FileParserFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace UniqueWordsCounter.Services.Impl
+{
+    public static class FileParserFactory
+    {
+        public static IFileParser GetInstance(string filePath)
+        {
+            var fileExtension = Path.GetExtension(filePath);
+
+            return fileExtension switch
+            {
+                ".txt" => new TxtFileParser(filePath),
+                _ => throw new Exception($"File with extension {fileExtension} isn't supported")
+            };
+        }
+    }
+}
