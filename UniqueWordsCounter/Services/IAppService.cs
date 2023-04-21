e51673ae00b2d8ca1ecb979575cf4bc5bf329fcc
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-21 10:23:27 +0300

    cleaned up a bit

--- UniqueWordsCounter/Services/IAppService.cs
+++ UniqueWordsCounter/Services/IAppService.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UniqueWordsCounter.Services
+﻿namespace UniqueWordsCounter.Services
 {
-    internal interface IAppService
+    public interface IAppService
     {
         void Run(string filePath, string resultFilePath);
     }
