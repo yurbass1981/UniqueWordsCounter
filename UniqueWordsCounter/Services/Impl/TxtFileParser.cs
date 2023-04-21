c767ac197c3312ee9121b9d6689545dbf7ec5a71
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-21 10:44:05 +0300

    corrected code

---
+++ UniqueWordsCounter/Services/Impl/TxtFileParser.cs
@@ -0,0 +1,16 @@
+﻿namespace UniqueWordsCounter.Services.Impl
+{
+    public class TxtFileParser : IFileParser
+    {
+        private readonly string filePath;
+
+        public TxtFileParser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public IEnumerable<string> GetLines()
+        {
+            return File.ReadLines(filePath).ToList();
+        }
+    }
+}
